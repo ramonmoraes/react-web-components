e0dfb155484a2be479d65f211a93c290d3476320
Ramon Moraes <ramon.moraes94@gmail.com>
2018-02-14 18:01:23 -0300

    initial commit with side-menu

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import MenuMobile from './components/MenuMobile';
+
+class App extends Component {
+render() {
+return  (
+    <div>
+        <MenuMobile>
+            <button> Oi </button>
+            <button> Oi </button>
+            <button> Oi </button>
+        </MenuMobile>
+        <p> Ola galera </p>
+    </div>
+    );
+}
+}
+export default App;
---
+++ src/components/MenuMobile/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+class MenuMobile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: true || this.props.active,
+        }
+    }
+
+    hideMenu =()=> {
+        this.setState({
+            active:false
+        })
+    };
+
+    showMenu =()=> {
+        console.log('oi');
+        
+        this.setState({
+            active: true,
+        })
+    };
+
+    navStopPropagation(ev) {
+        ev.stopPropagation();
+    }
+
+    render() {
+        const { active } = this.state;
+
+        const listOfButtons = (
+            this.props.children.length ? this.props.children : [ this.props.children ]
+        );
+        
+        const containerClass = ( active ) ? 'side-nav-container side-nav-container-overlay' : 'side-nav-container';
+        const navClass = 'side-nav ' + (( active ) ? 'side-nav-enabled' : 'side-nav-disabled');
+        
+        return (
+            <nav className='sticky-top'>
+                <div className ='top-menu'>
+                    <button onClick={this.showMenu}> <b> MENU </b> </button>
+                </div>
+                <div className={containerClass} onClick ={this.hideMenu}>
+                    <div className={navClass} onClick={this.navStopPropagation}>
+
+                        <header>
+                            <h1 className='datClass'>Header</h1>
+                        </header>
+
+                        <div className='btn-wrapper'>
+                            {listOfButtons.map(menuButton => menuButton)}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+export default MenuMobile;
