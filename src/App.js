0588666c1c824d5a986122fcf46fe58096975570
Ramon Moraes <ramon.moraes94@gmail.com>
2018-02-25 20:10:10 -0300

    add loading wrapper

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import MenuMobile from './components/MenuMobile';
+import LoadingWrapper from './components/LoadingWrapper';
+
 
 class App extends Component {
 render() {
+    // <LoadingWrapper src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Big_Pinnacle_of_Pilot_Mountain_high_res.jpg"/>
 return  (
     <div>
-        <MenuMobile>
+        <MenuMobile active="false">
             <button> Oi </button>
             <button> Oi </button>
             <button> Oi </button>
         </MenuMobile>
-        <p> Ola galera </p>
+        <p> Olá</p>
+
+        <LoadingWrapper>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Big_Pinnacle_of_Pilot_Mountain_high_res.jpg"/>
+        </LoadingWrapper>
     </div>
     );
 }
