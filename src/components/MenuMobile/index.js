a769442409a6b02354bbfbacd25961de51cb2818
Ramon Moraes <ramon.moraes94@gmail.com>
2018-04-07 21:27:02 -0300

    lint on app.ks

--- src/components/MenuMobile/index.js
+++ src/components/MenuMobile/index.js
@@ -1,62 +1,64 @@
-import React, { Component } from 'react';
-import './style.css';
+import React, { Component } from "react";
+import "./style.css";
 
 class MenuMobile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: (this.props.active) ? true : false,
-        }
-        
-    }
-
-    hideMenu =()=> {
-        this.setState({
-            active:false
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: this.props.active ? true : false
     };
+  }
 
-    showMenu =()=> {
-        console.log('oi');
-        
-        this.setState({
-            active: true,
-        })
-    };
+  hideMenu = () => {
+    this.setState({
+      active: false
+    });
+  };
 
-    navStopPropagation(ev) {
-        ev.stopPropagation();
-    }
+  showMenu = () => {
+    this.setState({
+      active: true
+    });
+  };
 
-    render() {
-        const { active } = this.state;
+  navStopPropagation(ev) {
+    ev.stopPropagation();
+  }
 
-        const listOfButtons = (
-            this.props.children.length ? this.props.children : [ this.props.children ]
-        );
-        
-        const containerClass = ( active ) ? 'side-nav-container side-nav-container-overlay' : 'side-nav-container';
-        const navClass = 'side-nav ' + (( active ) ? 'side-nav-enabled' : 'side-nav-disabled');
-        
-        return (
-            <nav className='sticky-top'>
-                <div className ='top-menu'>
-                    <button onClick={this.showMenu}> <b> MENU </b> </button>
-                </div>
-                <div className={containerClass} onClick ={this.hideMenu}>
-                    <div className={navClass} onClick={this.navStopPropagation}>
+  render() {
+    const { active } = this.state;
 
-                        <header>
-                            <h1 className='datClass'>Header</h1>
-                        </header>
+    const listOfButtons = this.props.children.length
+      ? this.props.children
+      : [this.props.children];
 
-                        <div className='btn-wrapper'>
-                            {listOfButtons.map(menuButton => menuButton)}
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
+    const containerClass = active
+      ? "side-nav-container side-nav-container-overlay"
+      : "side-nav-container";
+    const navClass =
+      "side-nav " + (active ? "side-nav-enabled" : "side-nav-disabled");
+
+    return (
+      <nav className="sticky-top">
+        <div className="top-menu">
+          <button onClick={this.showMenu}>
+            {" "}
+            <b> MENU </b>{" "}
+          </button>
+        </div>
+        <div className={containerClass} onClick={this.hideMenu}>
+          <div className={navClass} onClick={this.navStopPropagation}>
+            <header>
+              <h1 className="datClass">Header</h1>
+            </header>
+
+            <div className="btn-wrapper">
+              {listOfButtons.map(menuButton => menuButton)}
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 export default MenuMobile;
