b6664f2d5639166f99360b24fe383f915a706f59
Ramon Moraes <ramon.moraes94@gmail.com>
2018-04-28 22:11:49 -0300

    OverlayWrapper as stateless component

--- src/components/OverlayWrapper/index.js
+++ src/components/OverlayWrapper/index.js
@@ -2,15 +2,6 @@ import React, { Component } from "react";
 import "./style.css";
 
 class OverlayWrapper extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { children: null };
-  }
-
-  componentDidMount() {
-    this.setState({ children: this.props.children });
-  }
-
   handleClick = ev => {
     ev.stopPropagation();
     this.props.handleClick(ev);
@@ -18,9 +9,7 @@ class OverlayWrapper extends Component {
 
   render() {
     return (
-      <div className="Overlay" onClick={this.handleClick}>
-        {this.state.children}
-      </div>
+      <div className="Overlay" onClick={this.handleClick}></div>
     );
   }
 }
